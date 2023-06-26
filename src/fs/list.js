bf6370ae856b600586dc94cf8d1fe6acfb218a70
Valera Kozhevnikov <kozevnikovvalera@mail.ru>
2023-06-26 17:14:50 +0300

    add list function

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,10 @@
+import { readdir } from "node:fs/promises";
+
 const list = async () => {
-    // Write your code here 
+    const files = await readdir('./files');
+    for (const file of files) {
+        console.log([file]);
+    }
 };
 
 await list();

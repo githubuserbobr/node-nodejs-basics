6acdad63445dc017fdd572746b0cae0daf519023
Valera Kozhevnikov <kozevnikovvalera@mail.ru>
2023-06-26 17:23:23 +0300

    feat: add read file function

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,13 @@
+import { readFile } from "node:fs/promises";
+
 const read = async () => {
-    // Write your code here 
+  try {
+    const content = await readFile("./files/fileToRead.txt", {encoding: 'utf-8'});
+    console.log(content);
+  } catch (error) {
+    console.log(error);
+    throw new Error("FS operation failed");
+  }
 };
 
 await read();

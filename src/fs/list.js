1ef4c7fc59d18d89ed255b18146dac92fd50c39e
Valera Kozhevnikov <kozevnikovvalera@mail.ru>
2023-06-26 17:18:34 +0300

    fix error-catching logic

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,10 +1,15 @@
 import { readdir } from "node:fs/promises";
 
 const list = async () => {
-    const files = await readdir('./files');
+    try {
+        const files = await readdir('./files');
     for (const file of files) {
         console.log([file]);
     }
+    } catch (error) {
+        console.log(error);
+        throw new Error('FS operation failed');
+    }    
 };
 
 await list();

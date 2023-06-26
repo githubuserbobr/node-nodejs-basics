c922db49b5891a0472b8f413af931076f8e365f9
Valera Kozhevnikov <kozevnikovvalera@mail.ru>
2023-06-26 16:26:49 +0300

    feat: add rename logic

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,7 @@
+import { rename as renameFile} from 'node:fs/promises';
+
 const rename = async () => {
-    // Write your code here 
+  await renameFile('./files/wrongFilename.txt', './files/properFilename.md')
 };
 
 await rename();

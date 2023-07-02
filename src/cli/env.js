576f01dff43af0220532a1bffca5b9fd2e6e196a
Valera <kozevnikovvalera@gmail.ru>
2023-07-02 14:44:33 +0300

    feat: create parseEnv function

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,16 @@
 const parseEnv = () => {
-    // Write your code here 
-};
+try {
+    let names = Object.getOwnPropertyNames(process.env);
+    let result = names.filter(item => item.startsWith('RSS_'));
+    if (result.length !== 0) {
+        result.forEach((item) => process.stdout.write(`${item}=${process.env[item]}; `));
+    }
+    else {
+        throw new Error('FS operation failed');
+    }
+} catch (error) {
+    console.log(error);
+    throw new Error('FS operation failed');
+}};
 
 parseEnv();

71714c58c972c0dec697569716d2c893bc2b8561
Valera <kozevnikovvalera@gmail.ru>
2023-07-02 15:07:05 +0300

    feat: create parseArgs function

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,18 @@
 const parseArgs = () => {
-    // Write your code here 
+    try {
+        if (process.argv.length > 2) {
+            const args = process.argv.slice(2);
+            for (let i = 0; i < args.length; i +=2) {
+                process.stdout.write(`${args[i]} is ${args[i + 1]}, `);
+            }
+        }
+        else {
+            throw new Error('FS operation failed');
+        }
+    } catch (error) {
+        console.log(error);
+        throw new Error('FS operation failed');
+    }
 };
 
 parseArgs();

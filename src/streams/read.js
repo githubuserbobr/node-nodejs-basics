05b4370af50668782007410e5654e9de6ef8a7cd
Valera <kozevnikovvalera@gmail.ru>
2023-07-16 22:13:21 +0300

    feat: create read.js function

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,10 @@
+import { pipeline } from 'node:stream/promises';
+import fs from 'node:fs';
+
 const read = async () => {
-    // Write your code here 
+    const rs = fs.createReadStream('./files/fileToRead.txt');
+    const ws = process.stdout;
+    await pipeline(rs, ws);
 };
 
 await read();

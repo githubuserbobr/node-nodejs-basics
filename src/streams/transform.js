136bc777be97b41bd3beab58426300894f50006e
Valera <kozevnikovvalera@gmail.ru>
2023-07-16 22:43:51 +0300

    feat: create transform function

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,28 @@
+import { Transform, pipeline } from 'node:stream';
+
 const transform = async () => {
-    // Write your code here 
+    const readable = process.stdin;
+    const writeable = process.stdout;
+
+    const transform = new Transform({
+        transform(chunk, enc, cb) {
+            const chunkStringifued = chunk.toString().trim();
+
+            const revesredChunk = chunkStringifued.split('').reverse().join('');
+
+            this.push(revesredChunk + '\n');
+
+            cb();
+        }
+    });
+    pipeline(
+        readable,
+        transform,
+        writeable,
+        err => {
+            console.log(err);
+        }
+    ) 
 };
 
 await transform();

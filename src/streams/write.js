8e2028349df42263889d5905e16ea9b773a9b6fe
Valera <kozevnikovvalera@gmail.ru>
2023-07-16 22:20:20 +0300

    feat: create write.js file

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,10 @@
+import fs from 'node:fs';
+import { pipeline } from 'node:stream/promises';
+
 const write = async () => {
-    // Write your code here 
+    const rs =  process.stdin;
+    const ws = fs.createWriteStream('./files/fileToWrite.txt');
+    await pipeline(rs, ws);
 };
 
 await write();
